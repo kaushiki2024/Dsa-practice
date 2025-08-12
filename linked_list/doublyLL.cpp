6d9c40b4353fe4be42e56fa004eaa1a36b3bb196
Kaushiki Mishra <kaushikim80@gmail.com>
2025-08-12 20:46:22 +0530

    Implement insert at any position function and also handled the NULL cases

--- linked_list/doublyLL.cpp
+++ linked_list/doublyLL.cpp
@@ -29,18 +29,67 @@ int getLength(Node* head){
     }
     return len;
 }
-void insertATHead(Node* &head, int d){
+void insertATHead(Node* &tail,Node* &head, int d){
+    if(head==NULL){
+        //empty list
+        Node* temp= new Node(d);
+        head = temp;
+        tail = temp;
+        return;
+    }
     Node* temp = new Node(d);
     temp->next = head;
     head->prev = temp;
     head  = temp;
 }
+void insertAtTail(Node* &tail,Node* &head,int d){
+    if(tail == NULL){
+        Node* temp = new Node(d);
+        tail = temp;
+        head = temp;
+        return;
+    }
+    Node* temp = new Node(d);
+    tail->next = temp;
+    temp->prev = tail;
+    tail = temp;
+}
+void insertAtposition(Node* &tail,Node* &head,int position,int d){
+    Node* temp = head;
+    if(position == 1){
+        insertATHead(tail,head,d);
+        return;
+    }
+    int cnt=1;
+    while(cnt < position-1){
+        temp = temp->next;
+        cnt++;
+    }
+    //inserting at last position
+    if(temp->next == NULL){
+        insertAtTail(tail,head,d);
+        return;
+    }
+    
+    Node* nodeToinsert = new Node(d);
+    nodeToinsert->next = temp->next;
+    temp->next->prev = nodeToinsert;
+    temp->next = nodeToinsert;
+    nodeToinsert->prev = temp;
+    
+        
+}
 int main(){
-    Node* node1 = new Node(10);
-    Node* head = node1;
+    Node* head = NULL;
+    Node* tail = NULL;
+    //cout<<getLength(head)<<endl;
+   
+
+    insertATHead(tail,head,11);
     print(head);
-    cout<<getLength(head)<<endl;
-    insertATHead(head,11);
+    insertAtTail(tail,head,16);
+    print(head);
+    insertAtposition(tail,head,2,12);
     print(head);
     return 0;
 }
