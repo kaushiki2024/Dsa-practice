3a040aec3d6619db16ad095f41746abe4099c7da
Kaushiki Mishra <kaushikim80@gmail.com>
2025-09-12 13:29:54 +0530

    Implementation of Hollow dimaond inscribed under rectangle

---
+++ Array/Hollow.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5-i;j++){
+            cout<<"*";
+        }
+        for(int j=0;j<2*i;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<5-i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+        for(int i=0;i<4;i++){
+        for(int j=0;j<i+2;j++){
+            cout<<"*";
+        }
+        for(int j=0;j<8-2*i-2;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<i+2;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
